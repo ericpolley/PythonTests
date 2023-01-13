6fd4af0f947e3c45129984c92f1c418a8d3536b0
eric polley <ericpolley@live.com>
2023-01-12 19:24:59 -0800

    window with count, button cycle, and link

--- index.py
+++ index.py
@@ -1,12 +1,24 @@
 import tkinter as tk
+import subprocess
+
+global score
+score = 0
+
+
+def run_game():
+    subprocess.run(["python", "pyglet_test02.py"])
 
 
 def firstbtnAct():
     hideAll()
+    countbtn.pack()
+    subbtn.pack()
     txt01.pack()
     button.pack()
     txt01.place(relx=.5, rely=.3, anchor="center")
+    countbtn.place(relx=.4, rely=.1, anchor="center")
     button.place(relx=.5, rely=.6, anchor="center")
+    subbtn.place(relx=.6, rely=.1, anchor="center")
 
 
 def secondbtnAct():
@@ -33,6 +45,18 @@ def fourthbtnAct():
     fourthbtn.place(relx=.5, rely=.6, anchor="center")
 
 
+def add():
+    global score
+    score += 1
+    txt01.config(text=score)
+
+
+def sub():
+    global score
+    score -= 1
+    txt01.config(text=score)
+
+
 def hideAll():
     txt01.pack_forget()
     txt02.pack_forget()
@@ -50,13 +74,24 @@ def hideAll():
     secondbtn.place_forget()
     thirdbtn.place_forget()
     fourthbtn.place_forget()
+    countbtn.place_forget()
+    subbtn.place_forget()
 
 
 root = tk.Tk()
 
 root.title("Game title the game")
-root.geometry("900x600")
+root.geometry("1000x600")
 root.config(bg='#BBBBBB')
+
+countbtn = tk.Button(root, text="add", command=add,  bg="red", font=(
+    "Helvetica", 32), justify="center", fg="white")
+countbtn.pack()
+countbtn.place(relx=.4, rely=.1, anchor="center")
+subbtn = tk.Button(root, text="subtract", command=sub,  bg="red", font=(
+    "Helvetica", 32), justify="center", fg="white")
+subbtn.pack()
+subbtn.place(relx=.6, rely=.1, anchor="center")
 
 button = tk.Button(root, text="Start", command=secondbtnAct,
                    bg="blue", font=("Helvetica", 32), justify="center", fg="white")
@@ -66,7 +101,7 @@ thirdbtn = tk.Button(root, text="Continue", command=fourthbtnAct, bg="red", font
     "Helvetica", 32), justify="center", fg="white")
 fourthbtn = tk.Button(root, text="Okay", command=firstbtnAct, bg="orange", font=(
     "Helvetica", 32), justify="center", fg="black")
-txt01 = tk.Label(root, text="Game Title - the game", bg="#BBBBFF", font=(
+txt01 = tk.Label(root, text=score, bg="#BBBBFF", font=(
     "Helvetica", 32))
 
 txt02 = tk.Label(root, text="What is red?", bg="#BBBBBB", font=(
@@ -81,4 +116,8 @@ txt01.place(relx=.5, rely=.3, anchor="center")
 button.pack()
 button.place(relx=.5, rely=.6, anchor="center")
 
+game_button = tk.Button(root, text="Open Game", command=run_game, bg="red", font=(
+    "Helvetica", 32), justify="center", fg="white")
+game_button.pack()
+game_button.place(relx=.5, rely=.9, anchor="center")
 root.mainloop()
